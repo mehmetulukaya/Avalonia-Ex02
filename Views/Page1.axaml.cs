5272e4c6e9c417d14920c5e7be4439185c857b49
Mehmet Ulukaya <mehmet.ulukaya@tr.panasonic.com>
2020-10-06 10:43:50 +0300

    TabControl added and Page1 and 2

---
+++ Views/Page1.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Avalonia_Ex02.Views
+{
+    public class Page1 : UserControl
+{
+    public Page1()
+    {
+        this.InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
+}
---
+++ Views/Page2.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Avalonia_Ex02.Views
+{
+    public class Page2 : UserControl
+{
+    public Page2()
+    {
+        this.InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
+}
